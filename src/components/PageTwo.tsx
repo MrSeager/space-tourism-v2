b509af994d5bc152ce0cb4a67508a39a747f2601
MrSeager <seager1993@gmail.com>
2025-01-26 11:44:18 +0200

    1/26/2025

--- src/components/PageTwo.tsx
+++ src/components/PageTwo.tsx
@@ -1,9 +1,9 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 //Bootstrap
 import { Container, Row, Col, Button, Image, ButtonGroup } from 'react-bootstrap';
 //Spring
-import { useSpring, animated } from '@react-spring/web';
-import { useSlide } from './animation.tsx';
+import { useTransition, animated } from '@react-spring/web';
+import { useSlide, useFadeTransition } from './animation.tsx';
 
 interface DestinationsProps { 
     name: string; 
@@ -25,6 +25,36 @@ const PageTwo: FC<PageTwoProps> = ({ destinationData }) => {
     const slideLift = useSlide('-');
     const slideRight = useSlide('+');
 
+    const [itemsImage, setItemsImage] = useState([
+        { id: dest, src: destinationData[dest].images.webp, name: destinationData[dest].name }
+    ]);
+    
+    useEffect(() => {
+        setItemsImage([{ id: dest, src: destinationData[dest].images.webp, name: destinationData[dest].name }]);
+    }, [dest]);
+    
+    const transitionsImage = useFadeTransition(itemsImage);
+    
+    const [itemsText, setItemsText] = useState([{
+        id: dest,
+        name: destinationData[dest].name,
+        description: destinationData[dest].description,
+        distance: destinationData[dest].distance,
+        travel: destinationData[dest].travel
+    }]);
+    
+    useEffect(() => {
+        setItemsText([{
+          id: dest,
+          name: destinationData[dest].name,
+          description: destinationData[dest].description,
+          distance: destinationData[dest].distance,
+          travel: destinationData[dest].travel
+        }]);
+    }, [dest]);
+    
+    const transitionsText = useFadeTransition(itemsText);
+
     return (
         <Container fluid className='cs-bg-page-two min-vh-100 px-lg-5 px-0 d-flex flex-column justify-content-start'>
             <animated.div style={slideLift}>
@@ -34,13 +64,17 @@ const PageTwo: FC<PageTwoProps> = ({ destinationData }) => {
                 </Container>
             </animated.div>
             <Row className='mx-0'>
-                <Col lg={7} xs={12} className='d-flex flex-column justify-content-center align-items-center'>
-                    <animated.div style={slideLift} className='d-flex flex-column justify-content-center align-items-center'>
-                        <Image fluid
-                            src={`https://raw.githubusercontent.com/MrSeager/space-tourism-v2/refs/heads/main/src/starter-code/` + destinationData[dest].images.webp.replace("./", "")}
-                            alt={destinationData[dest].name + ' image'}
-                            className='h-75'
-                        />
+                <Col lg={7} xs={12} className='cs-min-h d-flex flex-column align-items-center justify-content-center'>
+                    <animated.div style={slideLift} className='w-50 position-relative overflow-hidden'>
+                    {transitionsImage((style, item) => (
+                        <animated.div style={style} key={item.id} className={`${item.id === dest ? '' : 'position-absolute'} m-auto`}>
+                            <Image fluid
+                                src={`https://raw.githubusercontent.com/MrSeager/space-tourism-v2/refs/heads/main/src/starter-code/` + item.src.replace("./", "")}
+                                alt={item.name + ' image'}
+                                className='spin'
+                            />
+                        </animated.div>
+                    ))}
                     </animated.div>
                 </Col>
                 <Col lg={5} xs={12} >
@@ -55,19 +89,23 @@ const PageTwo: FC<PageTwoProps> = ({ destinationData }) => {
                                 </Button>
                             ))}
                         </ButtonGroup>
-                        <Container className='px-0 text-white text-lg-start text-center d-flex flex-column align-items-lg-start align-items-center justify-content-between'>
-                            <h3 className='cs-ff-bellefair px-0 cs-fs text-uppercase'>{destinationData[dest].name}</h3>
-                            <p className='fs-5 cs-fw-300 cs-fw-300'>{destinationData[dest].description}</p>
-                            <Row className='w-100 border-top pt-3 mt-4 gap-sm-0 gap-3'>
-                                <Col sm={6} xs={12}>
+                        <Container className='position-relative px-0 text-white text-lg-start text-center d-flex flex-column align-items-lg-start align-items-center justify-content-between'>
+                        {transitionsText((style, item) => (
+                            <animated.div style={style} key={item.id}>
+                                <h3 className='cs-ff-bellefair px-0 cs-fs text-uppercase'>{item.name}</h3>
+                                <p className='cs-four-five-line-p fs-5 cs-fw-300'>{item.description}</p>
+                                <Row className='w-100 border-top pt-3 mt-4 gap-sm-0 gap-3'>
+                                    <Col sm={6} xs={12}>
                                     <h4 className='text-uppercase h6 cs-ls cs-fw-300'>Avg. distance</h4>
-                                    <h4 className='h2 text-uppercase cs-ff-bellefair cs-fw-300'>{destinationData[dest].distance}</h4>
-                                </Col>
-                                <Col sm={6} xs={12}>
+                                    <h4 className='h2 text-uppercase cs-ff-bellefair cs-fw-300'>{item.distance}</h4>
+                                    </Col>
+                                    <Col sm={6} xs={12}>
                                     <h4 className='text-uppercase h6 cs-ls cs-fw-300'>Est. travel time</h4>
-                                    <h4 className='h2 text-uppercase cs-ff-bellefair cs-fw-300'>{destinationData[dest].travel}</h4>
-                                </Col>
-                            </Row>
+                                    <h4 className='h2 text-uppercase cs-ff-bellefair cs-fw-300'>{item.travel}</h4>
+                                    </Col>
+                                </Row>
+                            </animated.div>
+                        ))}
                         </Container>
                     </animated.div>
                 </Col>
--- src/components/animation.tsx
+++ src/components/animation.tsx
@@ -1,11 +1,21 @@
 // animations.tsx
-import { useSpring, AnimatedProps } from 'react-spring';
+import { useSpring, useTransition, AnimatedProps } from 'react-spring';
 
 export const useSlide = (sign: string): AnimatedProps<{ opacity: number; x: string }> => {
     const direction = sign === '+' ? '250px' : '-250px';
+    
     return useSpring({
         from: { opacity: 0, x: direction },
         to: { opacity: 1, x: '0px' },
         config: { tension: 75, friction: 10 },
+    });
+};
+
+export const useFadeTransition = (items: any[]) => {
+    return useTransition(items, {
+        from: { opacity: 0, position: 'absolute' },
+        enter: { opacity: 1, position: 'relative' },
+        leave: { opacity: 0, position: 'absolute' },
+        config: { duration: 500 }
     });
 };
